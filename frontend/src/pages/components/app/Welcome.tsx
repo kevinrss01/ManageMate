b2353fb03f994990480936e911c12dc79aa31c63
Kevin Rousseau <kevin.rousseau@eurostep.com>
2023-03-15 16:58:17 +0100

    css progress in homepage of app

--- frontend/src/pages/components/app/Welcome.tsx
+++ frontend/src/pages/components/app/Welcome.tsx
@@ -1,13 +1,43 @@
 import { MdWavingHand } from "react-icons/md";
+import { useState } from "react";
+import { SlMagnifier } from "react-icons/sl";
 
 export default function Welcome() {
+  const [firstName, setFirstName] = useState("Kevin");
+  const [lastName, setLastName] = useState("Rousseau");
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchFiles = () => {
+    if (searchValue) {
+      console.log(searchValue);
+    }
+  };
+
   return (
     <>
       <div className="welcomeContainer">
         <div className="welcomeTextContainer">
           <h2>
-            Bienvenue Prénom Nom ! <MdWavingHand />
+            Bienvenue {firstName + " " + lastName} !{" "}
+            <MdWavingHand style={{ marginLeft: "5px" }} />
           </h2>
+        </div>
+        <div className="searchContainer">
+          <SlMagnifier className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Rechercher quelque chose ici"
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (searchValue) {
+                  searchFiles();
+                }
+              }
+            }}
+          />
         </div>
       </div>
     </>
